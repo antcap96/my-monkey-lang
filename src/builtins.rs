8cd26f1dc3a0a487874fb64b9b871746f6af9a7e
António Capela <acapela96@hotmail.com>
2023-07-14 22:12:20 +0100

    add builtin functions to handle arrays

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,16 +1,53 @@
-use crate::object::{EvaluationError, Object, ObjectCore, QuickReturn, BuiltinFunction};
+use crate::object::{BuiltinFunction, EvaluationError, Object, ObjectCore, QuickReturn};
+
+fn unexpected_number_of_arguments_error(expected: usize, got: usize) -> QuickReturn {
+    QuickReturn::Error(EvaluationError::BuiltinFunctionError(format!(
+        "unexpected number of arguments. Expected {} got {}",
+        expected, got
+    )))
+}
+
+fn builtin_first(args: Vec<Object>) -> Result<Object, QuickReturn> {
+    if args.len() != 1 {
+        return Err(unexpected_number_of_arguments_error(1, args.len()));
+    }
+    match args[0].core_ref() {
+        ObjectCore::Array(arr) => {
+            if arr.len() == 0 {
+                return Ok(Object::null());
+            }
+            Ok(arr[0].clone())
+        }
+        _ => Err(QuickReturn::Error(EvaluationError::BuiltinFunctionError(
+            format!("unexpected argument type. Expected Array got {:?}", args[0]),
+        ))),
+    }
+}
+
+fn builtin_last(args: Vec<Object>) -> Result<Object, QuickReturn> {
+    if args.len() != 1 {
+        return Err(unexpected_number_of_arguments_error(1, args.len()));
+    }
+    match args[0].core_ref() {
+        ObjectCore::Array(arr) => {
+            if arr.len() == 0 {
+                return Ok(Object::null());
+            }
+            Ok(arr[arr.len() - 1].clone())
+        }
+        _ => Err(QuickReturn::Error(EvaluationError::BuiltinFunctionError(
+            format!("unexpected argument type. Expected Array got {:?}", args[0]),
+        ))),
+    }
+}
 
 fn builtin_len(args: Vec<Object>) -> Result<Object, QuickReturn> {
     if args.len() != 1 {
-        return Err(QuickReturn::Error(EvaluationError::BuiltinFunctionError(
-            format!(
-                "unexpected number of arguments. Expected 1 got {}",
-                args.len()
-            ),
-        )));
+        return Err(unexpected_number_of_arguments_error(1, args.len()));
     }
     match args[0].core_ref() {
         ObjectCore::String(s) => Ok(Object::integer(s.len() as i64)),
+        ObjectCore::Array(arr) => Ok(Object::integer(arr.len() as i64)),
         _ => Err(QuickReturn::Error(EvaluationError::BuiltinFunctionError(
             format!(
                 "unexpected argument type. Expected String got {:?}",
@@ -20,9 +57,48 @@ fn builtin_len(args: Vec<Object>) -> Result<Object, QuickReturn> {
     }
 }
 
+fn builtin_push(args: Vec<Object>) -> Result<Object, QuickReturn> {
+    if args.len() != 2 {
+        return Err(unexpected_number_of_arguments_error(2, args.len()));
+    }
+    match args[0].core_ref() {
+        ObjectCore::Array(arr) => {
+            let mut new_arr = arr.clone();
+            new_arr.push(args[1].clone());
+            Ok(Object::array(new_arr))
+        }
+        _ => Err(QuickReturn::Error(EvaluationError::BuiltinFunctionError(
+            format!("unexpected argument type. Expected Array got {:?}", args[0]),
+        ))),
+    }
+}
+
+fn builtin_tail(args: Vec<Object>) -> Result<Object, QuickReturn> {
+    if args.len() != 1 {
+        return Err(unexpected_number_of_arguments_error(1, args.len()));
+    }
+    match args[0].core_ref() {
+        ObjectCore::Array(arr) => {
+            if arr.len() == 0 {
+                return Ok(Object::null());
+            }
+            Ok(Object::array(arr[1..].to_owned()))
+        }
+        _ => Err(QuickReturn::Error(EvaluationError::BuiltinFunctionError(
+            format!("unexpected argument type. Expected Array got {:?}", args[0]),
+        ))),
+    }
+}
+
 pub(crate) fn map_builtins(s: &str) -> Option<BuiltinFunction> {
     match s {
+        "first" => Some(BuiltinFunction {
+            func: builtin_first,
+        }),
+        "last" => Some(BuiltinFunction { func: builtin_last }),
         "len" => Some(BuiltinFunction { func: builtin_len }),
+        "push" => Some(BuiltinFunction { func: builtin_push }),
+        "tail" => Some(BuiltinFunction { func: builtin_tail }),
         _ => None,
     }
 }
